4cd636253e49e4bbfa103df3676a4b8204b62ae7
calliauc <clement@calliau.io>
2023-07-14 01:07:47 +0200

    env

--- src/app/player/player-service.service.ts
+++ src/app/player/player-service.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Player } from './player';
 import { NewPlayer } from './new-player';
-import { Observable } from 'rxjs';
+import { env } from 'process';
 
 @Injectable({
   providedIn: 'root'
@@ -11,17 +11,18 @@ export class PlayerServiceService {
 
   url_docker: string = "http://backend:8080/players"
   url_local: string = "http://localhost:9000/players"
-  url_server: string = "http://192.168.0.80:8123/players"
+  url_server: string = "http://192.168.0.228:9000/players"
+  url: string = env["apiUrl"] as string;
 
   constructor(private http: HttpClient) { }
 
   public get_players() {
-    return this.http.get<Player[]>(this.url_local, {responseType: 'json'});
+    return this.http.get<Player[]>(this.url, {responseType: 'json'});
   }
 
   public create_player(player: NewPlayer){
     console.log(player);
-    return this.http.post<NewPlayer>(this.url_local, player, {responseType: 'json'}).subscribe();
+    return this.http.post<NewPlayer>(this.url, player, {responseType: 'json'}).subscribe();
   }
 
 }
