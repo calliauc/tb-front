57b8a43b0acd014c936319bb2630792066f27d39
calliauc <clement@calliau.io>
2023-07-14 11:04:18 +0200

    add url api

--- src/app/player/player-service.service.ts
+++ src/app/player/player-service.service.ts
@@ -8,19 +8,20 @@ import { NewPlayer } from './new-player';
 })
 export class PlayerServiceService {
 
-  url_docker: string = "http://backend:8080/players"
+  url_docker: string = "http://backend:9000/players"
   url_local: string = "http://localhost:9000/players"
   url_server: string = "http://192.168.0.228:9000/players"
+  url_distant: string = "https://api-trollball.makhai.xyz/players"
 
   constructor(private http: HttpClient) { }
 
   public get_players() {
-    return this.http.get<Player[]>(this.url_server, {responseType: 'json'});
+    return this.http.get<Player[]>(this.url_distant, {responseType: 'json'});
   }
 
   public create_player(player: NewPlayer){
     console.log(player);
-    return this.http.post<NewPlayer>(this.url_server, player, {responseType: 'json'}).subscribe();
+    return this.http.post<NewPlayer>(this.url_distant, player, {responseType: 'json'}).subscribe();
   }
 
 }
