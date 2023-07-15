92a7ec69432ed9a2dbc7c7a25911aa120b47526c
calliauc <clement@calliau.io>
2023-07-16 00:37:43 +0200

    envs

--- src/app/player/player-service.service.ts
+++ src/app/player/player-service.service.ts
@@ -2,26 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Player } from './player';
 import { NewPlayer } from './new-player';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerServiceService {
 
-  url_docker: string = "http://backend:9000/players"
-  url_local: string = "http://localhost:9000/players"
-  url_server: string = "http://192.168.0.228:9000/players"
-  url_distant: string = "https://api-trollball.makhai.xyz/players"
 
   constructor(private http: HttpClient) { }
 
+  URL_API: string = environment.API_URL;
+  ENDPOINT: string = "/players";
+
+  URL: string = this.URL_API + this.ENDPOINT;
+
   public get_players() {
-    return this.http.get<Player[]>(this.url_distant, {responseType: 'json'});
+    return this.http.get<Player[]>(this.URL, {responseType: 'json'});
   }
 
   public create_player(player: NewPlayer){
     console.log(player);
-    return this.http.post<NewPlayer>(this.url_distant, player, {responseType: 'json'}).subscribe();
+    return this.http.post<NewPlayer>(this.URL, player, {responseType: 'json'}).subscribe();
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,8 @@
 
 export const environment = {
   production: false,
+  API_URL: "http://localhost:9000"
+
 };
 
 /*
